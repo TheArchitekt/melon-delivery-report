dbf74b99f26ee712f3b7916b69d72a24c4e9f60f
TheArchitekt <richardpennymon@gmail.com>
2022-11-02 19:13:28 -0400

    Add Comments

--- produce_summary.py
+++ produce_summary.py
@@ -1,17 +1,17 @@
-def number_of_melons(day, filepath):
+def number_of_melons(day, filepath): # Function takes a day and filepath in place of parameters
     """Iterates through a list a prints a report for each day"""
 
-    print(f"Day {day}")
-    melon_report = open(filepath)
+    print(f"Day {day}") # Prints the numbe rof the day
+    melon_report = open(filepath) # Opens the log file
 
 
-    for line in melon_report:
-        line = line.rstrip()
-        words = line.split('|')
+    for line in melon_report: # Iterates through each line in log file
+        line = line.rstrip() # Strips whitespace from right side
+        words = line.split('|') # Creates new list
 
-        melon = words[0]
-        count = words[1]
-        amount = words[2]
+        melon = words[0] # Item from list
+        count = words[1] # Item from list
+        amount = words[2] # Item from list
 
-        print(f"Delivered {count} {melon}s for total of ${amount}")
-    melon_report.close()
+        print(f"Delivered {count} {melon}s for total of ${amount}") # Displays result
+    melon_report.close() # Closes log file
