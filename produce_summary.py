2d1f65086894bc02f63a6d64131c038644744292
TheArchitekt <richardpennymon@gmail.com>
2022-11-02 18:57:10 -0400

    Create Melon Report Variable Create Loop Through Melon Report

--- produce_summary.py
+++ produce_summary.py
@@ -40,3 +40,16 @@ for line in the_file:
 the_file.close()
 
 def number_of_melons(day, filepath):
+    print(f"Day {day}")
+    melon_report = open(filepath)
+
+
+    for line in melon_report:
+        line = line.rstrip()
+        words = line.split('|')
+
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
+
+        print(f"Delivered {count} {melon}s for total of ${amount}")
