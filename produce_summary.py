58f681b2d62b6eb6282d0662f47e3be3021a178e
TheArchitekt <richardpennymon@gmail.com>
2022-11-02 19:08:32 -0400

    Add Docstring Remove Redudnant Conditions

--- produce_summary.py
+++ produce_summary.py
@@ -1,45 +1,6 @@
-print("Day 1")
-the_file = open("um-deliveries-day-1.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
-
-
-print("Day 2")
-the_file = open("um-deliveries-day-2.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
-
-
-print("Day 3")
-the_file = open("um-deliveries-day-3.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
-
 def number_of_melons(day, filepath):
+    """Iterates through a list a prints a report for each day"""
+
     print(f"Day {day}")
     melon_report = open(filepath)
 
